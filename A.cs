8e2fe8cf1006c1967b6293cd2da37691ffac1a5d
MaksymMakovskyi <5402029@stud.nau.edu.ua>
2022-09-16 13:40:11 +0300

    commit
    commit

---
+++ A.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LabWork1
+{
+    internal class A
+    {
+        //всі змінні оголошені в класі
+        float a;
+        float b;
+        private float c;
+
+        public float C
+        {
+            //повернення значення властивості в метод Main
+            get { return c; }
+            //присвоєння властивості результату виконання дій над полями
+            set { c = (a += b) - 0.01f; }
+        }
+
+        public A(float a, float b) //конструктор
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public void ResultOfAction()
+        {
+            //виведення значення властивості в консоль
+            Console.WriteLine($"Результат обчислення: {c}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LabWork1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A action1 = new A(1.943f, 14.43f); //об'єкт класу А: "action1"
+            action1.C = 0; //присвоєння значення властивості "С"
+            float c1 = action1.C; //витягування значення властивості "С" і присвоєння його в змінну "с1"
+            //виведення значення властивості "c" в консоль
+            Console.WriteLine($"Результат обчислення: {c1}");
+
+            A action2 = new A(-23.46f, 17.16f);
+            action2.C = 0;
+            float c2 = action2.C;
+            Console.WriteLine($"Результат обчислення: {c2}");
+
+            A action3 = new A(149.147f, -96.478f);
+            action3.C = 0;
+            //виведення значення властивості "С" в консоль
+            //також можна реалізувати за доромогою метода
+            action3.ResultOfAction();
+        }
+    }
+}
